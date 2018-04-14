9798b9e29acf0005b6ac62213e37959a6582e803
Nikita Kurochkin <faust3719@gmail.com>
2018-04-14 20:13:47 +0700

    Fix

--- config/routes.php
+++ config/routes.php
@@ -4,10 +4,10 @@
 				'view/([0-9]+)' => 'serf/view/$1',
                 'create'  =>  'serf/create',
 				'serf' => 'serf/index',
-				'' => 'serf/index',
 				'auth' => 'profile/auth',
 				'reg' => 'profile/reg',
-				'uservalidation' => 'profile/validate'
+				'uservalidation' => 'profile/validate',
+				'' => 'serf/index'
 		);
 
 		?>
